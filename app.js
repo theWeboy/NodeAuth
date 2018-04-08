b1b4099360a15e92f78be1c60a751e591c829fc3
theWeboy <devdeepakbansal@gmail.com>
2018-04-08 23:43:32 +0530

    Middleware setup

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const port = 3000;
+
+// Route Files
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+// Init App
+const app = express();
+
+// View Engine
+app.engine('handlebars', exphbs({defaultlayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Express Messages
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+app.use('/', index);
+app.use('/users', users);
+
+app.listen(port, function(){
+    console.log('Server started on port '+port);
+});
+
